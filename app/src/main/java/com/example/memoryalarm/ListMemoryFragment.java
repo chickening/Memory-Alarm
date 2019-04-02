682bbcef55d0ced89ca8b19d64c95fb204d280f5
chickening182 <chickening182@gmail.com>
2019-04-02 10:08:14 +0900

    Add RecycleView Adapter Frame

--- app/src/main/java/com/example/memoryalarm/ListMemoryFragment.java
+++ app/src/main/java/com/example/memoryalarm/ListMemoryFragment.java
@@ -1,6 +1,7 @@
 package com.example.memoryalarm;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 
 /**
@@ -27,10 +30,55 @@ public class ListMemoryFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_content_list_memory, container, false);
         final String testsets[] = {"마인크래프트", "치킨", "피자"};
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.fragment_content_list_memory, testsets);
         RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recylerview_list_memory_list);
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter();
         return view;
     }
 
+}
+public class ListMemoryAdapter extends RecyclerView.Adapter<ListMemoryAdapter.ViewHolder> {
+
+    private ArrayList<String> mData = null ;
+
+    // 아이템 뷰를 저장하는 뷰홀더 클래스.
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        //TextView textView1 ;
+
+        ViewHolder(View itemView) {
+            super(itemView) ;
+
+            // 뷰 객체에 대한 참조. (hold strong reference)
+            //textView1 = itemView.findViewById(R.id.text1) ;
+        }
+    }
+
+    // 생성자에서 데이터 리스트 객체를 전달받음.
+    ListMemoryAdapter(ArrayList<String> list) {
+        mData = list ;
+    }
+
+    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
+    @Override
+    public ListMemoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
+
+        View view = inflater.inflate(R.layout.content_list_memory_element, parent, false) ;
+        ListMemoryAdapter.ViewHolder vh = new ListMemoryAdapter.ViewHolder(view) ;
+
+        return vh ;
+    }
+
+    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
+    @Override
+    public void onBindViewHolder(ListMemoryAdapter.ViewHolder holder, int position) {
+        String text = mData.get(position) ;
+        //holder.textView1.setText(text) ;
+    }
+
+    // getItemCount() - 전체 데이터 갯수 리턴.
+    @Override
+    public int getItemCount() {
+        return mData.size() ;
+    }
 }
